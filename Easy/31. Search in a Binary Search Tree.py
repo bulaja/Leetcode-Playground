701eff5ceebc1200cd78e1f96173886fbaa11f94
Dejan Bulaja <dejanb06@gmail.com>
2023-03-13 23:14:38 +0100

    Done Search in a BST

---
+++ Easy/31. Search in a Binary Search Tree.py
@@ -0,0 +1,46 @@
+# Done on Mar 13th, 2023
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr = root
+
+        while curr:
+            if curr.val == val:
+                return curr
+            else:
+                if curr.val > val:
+                    curr = curr.left
+                else:
+                     curr = curr.right
+
+        return curr
+            
+
+# Runtime 81 ms Beats 50.82% 
+# Memory 16.4 MB Beats 60.22%
+
+"""
+700. Search in a Binary Search Tree
+Easy
+
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+
+Example 1:
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+"""
